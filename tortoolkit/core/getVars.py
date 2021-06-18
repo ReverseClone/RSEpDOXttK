b85d02a5fbb8d706f46b4d1f945767635e3319db
ReverseClone <65848984+ReverseClone@users.noreply.github.com>
2021-06-18 13:25:18 +0530

    Add files via upload

---
+++ tortoolkit/core/getVars.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from ..consts.ExecVarsSample import ExecVars
+#from ..core.database_handle import TorToolkitDB
+from tortoolkit import SessionVars
+import os
+
+def get_val(variable):
+    return SessionVars.get_var(variable)
+
